aa17e6638fa3f4d3b1691ea9891c3a46f2fbb618
Eugene Berezin <berezin.nlp@gmail.com>
2019-06-09 14:14:21 -0700

    Page object model. Finished test

---
+++ Tumblr-XCUITest-demo/Tumblr-XCUITest-demoUITests/RegistrationScreens.swift
@@ -0,0 +1,89 @@
+//
+//  RegistrationScreens.swift
+//  Tumblr-XCUITest-demoUITests
+//
+//  Created by Eugene Berezin on 6/9/19.
+//  Copyright © 2019 Eugene Berezin. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class RegistrationScreens {
+    
+    static let tumblr = XCUIApplication(bundleIdentifier: "com.tumblr.tumblr")
+    
+    struct Buttons {
+        let done: XCUIElement
+        let account: XCUIElement
+    }
+    
+    struct TextFields {
+        let email: XCUIElement
+        let password: XCUIElement
+        let username: XCUIElement
+        
+    }
+    
+    struct Tabs {
+        let posts: XCUIElement
+    }
+    
+    
+    static let buttons = Buttons(done: tumblr.navigationBars["Tumblr.AccountCompletionView"].buttons["Done"],
+                                 account: tumblr.tabBars.buttons["Account"])
+    
+    static let textFields = TextFields(email: tumblr.textFields["email"],
+                                       password: tumblr.secureTextFields["password"],
+                                       username: tumblr.textFields["username"])
+    static let tabs = Tabs(posts: tumblr.buttons["Posts"])
+    
+    static func generateUniqueEmail() -> String {
+        let time = Date().timeIntervalSinceReferenceDate
+        let email = "awesometester+\(time)@mailsack.com"
+        return email
+    }
+    
+    static func generateUniqueUserName() -> String {
+        let time = Date().timeIntervalSinceReferenceDate
+        let userName = "awesomeTester+\(time)"
+        return userName
+        
+    }
+    
+    static func pickInterests() {
+        let howOldAreYouTextField = tumblr.textFields["How old are you?"]
+        tumblr.buttons["Get Started"].tap()
+        howOldAreYouTextField.tap()
+        howOldAreYouTextField.typeText("25")
+        tumblr.navigationBars["TMOnboardingNavigationWrapperView"].buttons["Next"].tap()
+        Thread.sleep(forTimeInterval: 5)
+        tumblr.collectionViews/*@START_MENU_TOKEN@*/.staticTexts["Curators"]/*[[".cells.staticTexts[\"Curators\"]",".staticTexts[\"Curators\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tumblr.collectionViews/*@START_MENU_TOKEN@*/.staticTexts["GIF art"]/*[[".cells.staticTexts[\"GIF art\"]",".staticTexts[\"GIF art\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tumblr.collectionViews/*@START_MENU_TOKEN@*/.staticTexts["Animation"]/*[[".cells.staticTexts[\"Animation\"]",".staticTexts[\"Animation\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tumblr.collectionViews/*@START_MENU_TOKEN@*/.staticTexts["Motion Graphics"]/*[[".cells.staticTexts[\"Motion Graphics\"]",".staticTexts[\"Motion Graphics\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tumblr.collectionViews/*@START_MENU_TOKEN@*/.staticTexts["Net Art"]/*[[".cells.staticTexts[\"Net Art\"]",".staticTexts[\"Net Art\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tumblr.navigationBars["Tap the things you like"].buttons["Next"].tap()
+        
+    }
+    
+    static func registerNewUser(userName: String, email: String, password: String) {
+        buttons.account.tap()
+        textFields.email.tap()
+        textFields.email.typeText(email)
+        textFields.password.tap()
+        textFields.password.typeText(password)
+        textFields.username.tap()
+        textFields.username.typeText(userName)
+        buttons.done.tap()
+        
+        
+    }
+    
+    static  func logOut(email: String) {
+        tumblr.navigationBars["Tumblr.BlogView"].buttons["account icons settings"].tap()
+        tumblr.tables/*@START_MENU_TOKEN@*/.staticTexts["General settings"]/*[[".cells.staticTexts[\"General settings\"]",".staticTexts[\"General settings\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tumblr.tables.staticTexts[email].tap()
+        tumblr.sheets["Are you sure you want to log out?"].buttons["Log out"].tap()
+    }
+}
--- Tumblr-XCUITest-demo/Tumblr-XCUITest-demoUITests/Tumblr_XCUITest_demoUITests.swift
+++ Tumblr-XCUITest-demo/Tumblr-XCUITest-demoUITests/Tumblr_XCUITest_demoUITests.swift
@@ -9,48 +9,48 @@
 import XCTest
 
 class Tumblr_XCUITest_demoUITests: XCTestCase {
+    
+    let tumblr = XCUIApplication(bundleIdentifier: "com.tumblr.tumblr")
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        
         continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-       
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        tumblr.launch()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+       tumblr.terminate()
     }
+    
 
-    func testExample() {
-        let tubblr = XCUIApplication(bundleIdentifier: "com.tumblr.tumblr")
-        let emailTextField = tubblr.textFields["email"]
-        let passwordTextField = tubblr.secureTextFields["password"]
-        let usernameTextField = tubblr.textFields["username"]
-        let doneButton = tubblr.navigationBars["Tumblr.AccountCompletionView"].buttons["Done"]
-        let accountBar = tubblr.tabBars.buttons["Account"]
-        tubblr.launch()
-        //Thread.sleep(forTimeInterval: 10)
-        accountBar.tap()
+    func testTubmlrNewUserRegistrationTest() {
+        let email = RegistrationScreens.generateUniqueEmail()
         
-        emailTextField.tap()
-        emailTextField.typeText("email@email.com")
-        tubblr.secureTextFields["password"].tap()
-        tubblr.textFields["username"].tap()
-        tubblr.navigationBars["Tumblr.AccountCompletionView"].buttons["Done"].tap()
-        
-        
-        
-        
+        if tumblr.buttons["Get Started"].exists {
+            RegistrationScreens.pickInterests()
        
+        }
+        guard RegistrationScreens.buttons.account.waitForExistence(timeout: 30) else {
+            XCTFail("Test did not move further from the interesis")
+            return
+        }
         
+        RegistrationScreens.registerNewUser(userName: RegistrationScreens.generateUniqueUserName(), email: email, password: "AwesomePassword123$")
         
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        guard RegistrationScreens.buttons.account.waitForExistence(timeout: 30) else {
+            XCTFail("Account is not visible")
+            return
+        }
+        
+        RegistrationScreens.buttons.account.tap()
+        guard RegistrationScreens.tabs.posts.waitForExistence(timeout: 30) else {
+            XCTFail("Posts is not visiblle")
+            return
+        }
+        XCTAssert(RegistrationScreens.tabs.posts.exists, "Posts tab is not visible")
+        Thread.sleep(forTimeInterval: 5)
+        RegistrationScreens.logOut(email: email)
+        
     }
 
 }
